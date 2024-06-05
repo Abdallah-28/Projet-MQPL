b536139254a1e4f683af75b0c31ae72379b94051
Abdallah-28 <abdallahtandia9@gmail.com>
2024-06-05 23:06:52 +0000

    fichier contenant les notification

---
+++ notification_strategy.py
@@ -0,0 +1,22 @@
+from typing import List
+
+class NotificationStrategy:
+    def envoyer(self, message: str, destinataires: List['Membre']):
+        raise NotImplementedError
+
+class EmailNotificationStrategy(NotificationStrategy):
+    def envoyer(self, message: str, destinataires: List['Membre']):
+        for destinataire in destinataires:
+            print(f"Envoyer un email à {destinataire.nom}: {message}")
+
+class SMSNotificationStrategy(NotificationStrategy):
+    def envoyer(self, message: str, destinataires: List['Membre']):
+        for destinataire in destinataires:
+            print(f"Envoyer un SMS à {destinataire.nom}: {message}")
+
+class NotificationContext:
+    def __init__(self, strategy: NotificationStrategy):
+        self.strategy = strategy
+
+    def notifier(self, message: str, destinataires: List['Membre']):
+        self.strategy.envoyer(message, destinataires)
